5f8e0b7f61a6a4d2d28dccf0c387fc55c75e2b0d
Steven Decker <swdecker@gmail.com>
2019-09-11 18:09:09 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,4 +1,15 @@
 # Code your solution here!
 def run_guessing_game
-  
+  target = random_number()
+  guess = user_input()
 end
+
+def user_input
+  gets.chomp
+end
+
+def random_number
+  rand(5)+1
+end
+
+
